cc33745187c01353430ab410ef9f65bddc82f72f
Vickie Ye <vye1116@gmail.com>
2016-04-10 21:39:57 -0400

    added some stuff setting up the transition probabilities

--- strens/learning/module.py
+++ strens/learning/module.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import choice
 
 from pybrain.structure.modules import Module
 from pybrain.rl.learners.valuebased.interface import ActionValueInterface
@@ -11,13 +12,20 @@ class ActionModule(Module, ActionValueInterfac):
     as well as the posterior distribution on T(s, a, s') and
     the ML estimates of E[R(s, a)]'''
 
-    def __init__(self, numStates, numActions):
+    def __init__(self, numStates, numActions, alphas=None):
         Module.__init__(self, 1, 1)
 
         self.numRows = numStates
         self.numColumns = numActions
         self.actionTable = np.zeros((numStates, numActions))
+
+        # TODO: set actual alphas
+        if alphas is None:
+            alphas = np.ones(numStates)
+        self.alphas = alphas
+        self.prior = DirichletPrior(self.alphas)
         self.transitionProbs = np.zeros((numStates, numActions, numStates))
+        self.initTransProbs()
 
     @property
     def numActions(self):
@@ -26,3 +34,19 @@ class ActionModule(Module, ActionValueInterfac):
     @property
     def numStates(self):
         return self.numRows
+
+    def initTransProbs(self):
+        for s in xrange(self.numStates):
+            for a in xrange(self.numActions):
+                self.actionTable[s,a,:] = np.random.dirichlet(self.alphas)
+
+    def _forwardImplementation(self, inbuf, outbuf):
+        '''update our tables and return the best action'''
+        # TODO: update our transition state counts and posterior
+        state = inbuf[0]
+        outbuf[0] = self.getMaxAction(state)
+
+    def getMaxAction(self, state):
+        possible = self.actionTable[state]
+        best = np.where(possible == max(possible))[0]
+        return choice(best)
--- strens/learning/priors.py
+++ strens/learning/priors.py
@@ -1,13 +1,17 @@
 import numpy as np
+from scipy.special import gamma
 
 
 class DirichletPrior(object):
 
-    def __init__(self, numStates, alphas):
-        assert len(alphas) == numStates,
-        "Length of alpha input vector does not match number of states."
-        self.numStates = numStates
+    def __init__(self, alphas):
         self.alphas = alphas
+
+    def likelihood(self, pis):
+        '''pis are the multinomial probabilities'''
+        unnormalized = np.product(np.power(pis, self.alphas-1))
+        beta = np.product(gamma(self.alphas)) / gamma(np.sum(self.alphas))
+        return unnormalized / beta
 
 
 class SparsePrior(object):
@@ -22,3 +26,5 @@ if __name__=="__main__":
     numStates = 3
     alphas = np.ones(numStates) * 1.0/numStates
     dp = DirichletPrior(3, alphas)
+    pis = np.ones(numStates) * 1.0/numStates
+    print dp.likelihood(pis)
